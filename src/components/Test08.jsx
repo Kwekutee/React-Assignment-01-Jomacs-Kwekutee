cd23ec4eadf0c9d2891656e8cbfb6b60a24b5989
Kweku acquah <kwekutee@gmail.com>
2025-05-03 16:03:34 +0100

    Final commit for my React JSX Assignment

--- src/components/Test08.jsx
+++ src/components/Test08.jsx
@@ -6,7 +6,14 @@ Use JSX to render a <ul> list of three favorite fruits.
 
 function Test08() {
   return (
-    // Your code here
+    <div>
+      <h2>My favourites fruits</h2>
+      <ul>
+        <li>Apple</li>
+        <li>Grapes</li>
+        <li>Lemon</li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/Test09.jsx
+++ src/components/Test09.jsx
@@ -8,7 +8,9 @@ function Test09() {
   const name = "Jomacs";
 
   return (
-    // Your code here
+    <div>
+      <button>Click {name}!</button>
+    </div>
   );
 }
 
